88fef0a9906ca201edc0f7c487407cd9d7aa423b
Kirill13079 <kirillzhenkevich13@gmail.com>
2022-04-16 18:54:59 +0300

    update

---
+++ CustomControls/CustomControls/Controls/Entrys/Behaviors/EntryEyelinerBackgroundBehavior.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+
+namespace CustomControls.Controls.Entrys.Behaviors
+{
+    public class EntryEyelinerBackgroundBehavior : Behavior<EntryEyeliner>
+    {
+        protected override void OnAttachedTo(EntryEyeliner bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.TextChanged += Bindable_Background;
+        }
+
+        protected override void OnDetachingFrom(EntryEyeliner bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.TextChanged -= Bindable_Background;
+        }
+
+        private void Bindable_Background(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue.Length == 0)
+            {
+                ((EntryEyeliner)sender).BackgroundColor = Color.Red;
+            }
+            else 
+            {
+                ((EntryEyeliner)sender).BackgroundColor = Color.Transparent;
+            }
+        }
+    }
+}
---
+++ CustomControls/CustomControls/Controls/Entrys/Behaviors/EntryEyelinerPrefixBehavior.cs
@@ -0,0 +1,43 @@
+﻿using Xamarin.Forms;
+
+namespace CustomControls.Controls.Entrys.Behaviors
+{
+    public  class EntryEyelinerPrefixBehavior : Behavior<EntryEyeliner>
+    {
+        public static readonly BindableProperty PrefixProperty 
+            = BindableProperty.Create(
+                nameof(Prefix),
+                typeof(string),
+                typeof(EntryEyeliner),
+                "+");
+
+        public string Prefix
+        {
+            get => (string)GetValue(PrefixProperty);
+            set => SetValue(PrefixProperty, value);
+        }
+
+        protected override void OnAttachedTo(EntryEyeliner bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            bindable.TextChanged += Bindable_TextChanged;
+        }
+
+        protected override void OnDetachingFrom(EntryEyeliner bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.TextChanged -= Bindable_TextChanged;
+        }
+
+        private void Bindable_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(e.NewTextValue)
+                && !e.NewTextValue.StartsWith(Prefix))
+            {
+                ((EntryEyeliner)sender).Text = $"{Prefix} {e.NewTextValue}";
+            }
+        }
+    }
+}
